33ac01f097e2343627493f33224cd5f89d8b22c5
skasi7 <skasi.7@gmail.com>
2014-06-06 09:46:29 +0200

    www.newpct.com plugin now download from www.newpct1.com

--- flexget/README.md
+++ flexget/README.md
@@ -1,12 +1,10 @@
 Instructions to install revised newpct plugin
 =============================================
+Due to the recent www.newpct.com ISP block, www.newpct1.com site has been enabled. Update your urlrewrite plugin in Flexget to be able to download again.
 
-- Apply the patch with the command:
-  > patch urlrewrite_newpct.py < urlrewrite_newpct.patch
-
-  urlrewrite_newpct.py file can be found at:
-    /usr/local/lib/python2.7/dist-packages/flexget/plugins/urlrewrite_newpct.py
-    (This is the debian path for Python 2.7, find yours).
+- Copy the new plugin to:
+  /usr/local/lib/python2.7/dist-packages/flexget/plugins/urlrewrite_newpct.py
+  (This is the debian path for Python 2.7, find yours).
 
 Instructions to install malformed HTML in formlogin patch
 =========================================================
@@ -28,23 +26,3 @@ Instructions to install listdir encoding patch
     /usr/local/lib/python2.7/dist-packages/flexget/plugins/input
     (This is the debian path for Python 2.7, find yours).
 
-Instructions to install newpct plugin for flexget (added in r3248)
-==================================================================
-
-- Copy the urlrewrite_newpct.py file to:
-  /usr/local/lib/python2.7/dist-packages/flexget/plugins/
-  (This is the debian instalation path for Python 2.7, find the appropiate for
-  your distribution)
-
-- Check the right plugin installation with the command:
-  > flexget --plugins
-
-- Apply the qualities patch with the command:
-  > patch qualities.py < qualities.py.patch
-
-  qualities.py file can be found at:
-    /usr/local/lib/python2.7/dist-packages/flexget/utils
-    (Again, this is the debian path for Python 2.7, find yours).
-
-- Change your configuration (config.yml) to use the installed plugin. A simple
-  example can be found within this directory.)
--- flexget/urlrewrite_newpct.py
+++ flexget/urlrewrite_newpct.py
@@ -1,44 +1,54 @@
-import urllib2
+from __future__ import unicode_literals, division, absolute_import
 import logging
-from plugin_urlrewriting import UrlRewritingError
-from flexget.plugin import *
-from flexget.utils.tools import urlopener
-from flexget.utils.soup import get_soup
 import re
-import mechanize
+
+from flexget import plugin
+from flexget.event import event
+from flexget.plugins.plugin_urlrewriting import UrlRewritingError
+from flexget.utils.requests import Session
+from flexget.utils.soup import get_soup
 
 log = logging.getLogger('newpct')
 
+requests = Session()
+requests.headers.update({'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'})
+
 
 class UrlRewriteNewPCT(object):
-  """NewPCT urlrewriter."""
+    """NewPCT urlrewriter."""
 
-  # urlrewriter API
-  def url_rewritable(self, task, entry):
-    url = entry['url']
-    if url.startswith('http://www.newpct.com/download/'):
-      return False
-    if url.startswith('http://www.newpct.com/') or url.startswith('http://newpct.com/'):
-      return True
-    return False
+    # urlrewriter API
+    def url_rewritable(self, task, entry):
+        url = entry['url']
+        if url.startswith('http://www.newpct.com/download/'):
+            return False
+        if url.startswith('http://www.newpct1.com/descargar/'):
+            return False
+        if url.startswith('http://www.newpct.com/') or url.startswith('http://newpct.com/'):
+            return True
+        if url.startswith('http://www.newpct1.com/') or url.startswith('http://newpct1.com/'):
+            return True
+        return False
 
-  # urlrewriter API
-  def url_rewrite(self, task, entry):
-    entry['url'] = self.parse_download_page(entry['url'])
+    # urlrewriter API
+    def url_rewrite(self, task, entry):
+        entry['url'] = self.parse_download_page(entry['url'])
 
-  @internet(log)
-  def parse_download_page(self, url):
-    txheaders = {'User-agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
-    req = urllib2.Request(url, None, txheaders)
-    page = urlopener(req, log)
-    try:
-      soup = get_soup(page)
-    except Exception, e:
-      raise UrlRewritingError(e)
-    down_link = soup.find('a', attrs={'href': re.compile("descargar/torrent/")})
-    if not down_link:
-      raise UrlRewritingError('Unable to locate download link from url %s' % url)
-    return down_link.get('href')
+    @plugin.internet(log)
+    def parse_download_page(self, url):
+        url = url.replace('newpct.com', 'newpct1.com')
+        page = requests.get(url)
+        try:
+            soup = get_soup(page.text)
+        except Exception as e:
+            raise UrlRewritingError(e)
+        torrent_id_prog = re.compile("'torrentID': '(\d+)'")
+        torrent_ids = soup.findAll(text=torrent_id_prog)
+        if len(torrent_ids) == 0:
+            raise UrlRewritingError('Unable to locate torrent ID from url %s' % url)
+        torrent_id = torrent_id_prog.search(torrent_ids[0]).group(1)
+        return 'http://www.newpct1.com/descargar/torrent/%s/dummy.html' % torrent_id
 
-register_plugin(UrlRewriteNewPCT, 'newpct', groups=['urlrewriter'])
-
+@event('plugin.register')
+def register_plugin():
+    plugin.register(UrlRewriteNewPCT, 'newpct', groups=['urlrewriter'], api_ver=2)
